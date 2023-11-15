75b2bf3dae5756e6a6f49e54f736d8a429000ee0
Fahil haneef <Fahilhaneef006@gmail.com>
2023-11-15 13:04:34 +0530

    production error resolved

--- joblistingapp/src/Constants/baseUrl.jsx
+++ joblistingapp/src/Constants/baseUrl.jsx
@@ -1,3 +1,3 @@
-const BASEURL='http://localhost:3000'
+const BASEURL='https://job-listing-app-backend.vercel.app'
 
 export default BASEURL
--- server/index.js
+++ server/index.js
@@ -1,41 +1,48 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-const env = require('dotenv').config();
-const mongoose = require('mongoose');
-const authRoutes=require('./Routes/auth');
-const jobRoutes=require('./Routes/job')
-const User=require('./models/user')
-const cors=require('cors')
-
+const express = require("express");
+const bodyparser = require("body-parser");
+const env = require("dotenv").config();
+const mongoose = require("mongoose");
+const authRoutes = require("./Routes/auth");
+const jobRoutes = require("./Routes/job");
+const User = require("./models/user");
+const cors = require("cors");
 
 const app = express();
 
-
-app.use(cors())
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(
+  cors({
+    origin: ["https://job-listing-jxf4s692x-fahil-hybrido.vercel.app"],
+    methods: ["POST", "GET", "PUT"],
+    credentials: true,
+  })
+);
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-
-
-
-app.get('/', (req, res) => {
-    console.log(res)
-    res.json({status: 'Success'})
-})
-
-app.get('/health', (req, res) => {
-    res.status(200).json({status: 'Ok', message: 'Server is up and running', Time: new Date()})
+app.get("/", (req, res) => {
+  console.log(res);
+  res.json({ status: "Success" });
 });
 
-app.use(authRoutes)
-app.use(jobRoutes) 
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "Ok",
+    message: "Server is up and running",
+    Time: new Date(),
+  });
+});
 
+app.use(authRoutes);
+app.use(jobRoutes);
 
 app.listen(process.env.PORT, () => {
-    mongoose.connect(process.env.MONGODB_URL,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true}).then(() => {
-        console.log('Server running successfully and connected to db')
-    }).catch(error => console.log(error))
-
-})
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Server running successfully and connected to db");
+    })
+    .catch((error) => console.log(error));
+});
